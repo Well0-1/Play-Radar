4e56771307cf32b1a790abae8e76faadd1d83767
Salih Akyüzoğlu <sakyuzogluh@gmail.com>
2024-09-03 01:46:51 +0300

    cors origin for bug

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "https://play-radar.vercel.app" }));
 app.use("/api", gameRoutes);
 
 const mongoURI = process.env.mongoURI;
